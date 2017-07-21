0c76b72206a6aac74f286fa7b3613352649852f7
Ravi Prakash <hraviprakash@gmail.com>
2017-07-20 22:00:46 -0400

    Add Index Controller and Index.html

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,13 +1,13 @@
-package com.devopsbuddy;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
-public class HelloWorldController {
+@Controller
+public class IndexController {
 	
-//	@RequestMapping("/")
-	public String sayHello(){
+	@RequestMapping("/")
+	public String home(){
 		return "index";
 	}
 
