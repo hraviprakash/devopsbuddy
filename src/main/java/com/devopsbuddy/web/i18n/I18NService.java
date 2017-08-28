ec93a9f33598d3dc0ac0b4b29b7273c31e54b35c
Ravi Prakash <hraviprakash@gmail.com>
2017-08-27 23:09:29 -0400

    Add logging to the application

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -2,6 +2,8 @@ package com.devopsbuddy.web.i18n;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -14,6 +16,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class I18NService {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(I18NService.class);
+
 
 	@Autowired MessageSource messageSource;
 	
@@ -24,6 +30,7 @@ public class I18NService {
 	 * @return
 	 */
 	public String getMessage(String messageId){
+		log.info("Returning i18N message id {}", messageId);
 		Locale locale = LocaleContextHolder.getLocale();
 		return getMessage(messageId, locale);
 	}
